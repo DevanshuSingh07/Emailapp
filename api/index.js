b0f5d7fdc4c029bd92dcbf0f48fc9f9c138272be
devanshu singh <dsdevanshu7@gmail.com>
2025-02-07 15:59:24 +0530

    "updated url variable change"

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const url =process.env.scriptUrl 
+const url =process.env.script_Url 
 
 // Middleware
 app.use(cors());
